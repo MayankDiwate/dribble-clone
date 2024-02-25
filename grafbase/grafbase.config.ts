51049004fed53daa2a60e3bc04d8b195e41abc46
Mayank Diwate <MayankDiwate007@gmail.com>
2024-02-25 17:56:00 +0530

    Added Grafbase Models

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,41 +1,58 @@
-import { config, graph } from '@grafbase/sdk'
+import { auth, config, graph } from "@grafbase/sdk";
 
-const g = graph.Standalone()
+const g = graph.Standalone();
 
 // @ts-ignore
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
+const user = g.model("User", {
+  name: g.string().length({ min: 2, max: 20 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
   description: g.string().length({ min: 2, max: 1000 }).optional(),
   githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
-  // @ts-ignore
-  projects: g.relation(() => Project).list().optional(),
-  // @ts-ignore
-}).auth((rules) => {
-  rules.public().read()
-})
+  linkedinUrl: g.url().optional(),
+});
 
 // @ts-ignore
-const Project = g.model('Project', {
+const project = g.model("Project", {
   title: g.string().length({ min: 3 }),
-  description: g.string(), 
+  description: g.string(),
   image: g.url(),
-  liveSiteUrl: g.url(), 
-  githubUrl: g.url(), 
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
   category: g.string().search(),
-  // @ts-ignore
-  createdBy: g.relation(() => User),
-  // @ts-ignore
-}).auth((rules) => {
-  rules.public().read()
-  rules.private().create().delete().update()
-})
+  createdBy: g.ref(user),
+});
 
+user.projects = g.ref(project);
 
+const jwt = auth.JWT({
+  issuer: "grafbase",
+  secret: g.env("NEXTAUTH_SECRET"),
+});
 
-export default config({
+config({
   schema: g,
-})
-
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private(),
+  },
+});
+config({
+  schema: g,
+  auth: {
+    providers: [project],
+    rules: (rules) => {
+      rules.public().read();
+    },
+  },
+});
+config({
+  schema: g,
+  auth: {
+    providers: [user],
+    rules: (rules) => {
+      rules.public().read();
+      rules.private().create().delete().update();
+    },
+  },
+});
